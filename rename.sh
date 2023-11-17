b5a13810c1c0683e4638db10461b47f2a56f339f
Jimno <98581961+DerJimno@users.noreply.github.com>
2023-11-17 23:08:48 +0100

    make it ignore the file extension (since we have png, jpg, xpm...)

--- rename.sh
+++ rename.sh
@@ -1,1 +1,1 @@
-ls | cat -n | while read n f; do mv "$f" `printf "%04d.jpg" $(($n-1))` 2>/dev/null; done
+ls | cat -n | while read n f; do mv "$f" `printf "%04d.%s" $(($n-1)) "${f##*.}"` 2>/dev/null; done
